9151ff8e4980ca4ce62448b85771406f90a85ff3
Sanglap Mridha <sanglapmridha@gmail.com>
2024-03-05 22:13:47 +0530

    feat: add new sections

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,3 +1,8 @@
 export { default as CustomerReviews } from "./customerReviews/CustomerReviews";
 export { default as Footer } from "./footer/Footer";
 export { default as Hero } from "./hero/Hero";
+export { default as PopularProducts } from "./popularProducts/PopularProducts";
+export { default as SuperQuality } from "./superQuality/SuperQuality";
+export { default as Services } from "./services/Services";
+export { default as SpecialOffer } from "./specialOffer/SpecialOffer";
+export { default as Subscribe } from "./subscribe/Subscribe";
