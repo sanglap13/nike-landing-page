48bcf7383384081c1cb73360f0cde74f8793569c
Sanglap Mridha <sanglapmridha@gmail.com>
2024-03-05 22:07:35 +0530

    feat: add sections and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
+import { Home } from "./components/pages";
+
+const App = () => <Home />;
 
 export default App;
---
+++ src/components/sections/index.ts
@@ -0,0 +1,3 @@
+export { default as CustomerReviews } from "./customerReviews/CustomerReviews";
+export { default as Footer } from "./footer/Footer";
+export { default as Hero } from "./hero/Hero";
