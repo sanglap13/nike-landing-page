17e3a71973ca257826f9d5486b7b05609bf72c7e
Sanglap Mridha <sanglapmridha@gmail.com>
2024-03-06 14:21:04 +0530

    refactor: change some component

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,22 +1,47 @@
 import React from "react";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "../../sections";
+import { Navbar } from "../../shared";
 
 const Home = () => {
   return (
     <main className="relative">
-      Nav
+      <Navbar />
       <section
         className="xl:padding-1 
   wide:padding-r padding-b"
       >
-        Hero
+        <Hero />
       </section>
-      <section className=" padding">PopularProducts</section>
-      <section className=" padding">SuperQuality</section>
-      <section className=" padding-x py-10">Services</section>
-      <section className=" padding">SpecialOffer</section>
-      <section className=" padding bg-pale-blue">CustomerReviews</section>
-      <section className=" padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <section className=" padding-x padding-t pb-8 bg-black">Footer</section>
+      <section className=" padding">
+        <PopularProducts />
+      </section>
+      <section className=" padding">
+        <SuperQuality />
+      </section>
+      <section className=" padding-x py-10">
+        <Services />
+      </section>
+      <section className=" padding">
+        <SpecialOffer />
+      </section>
+      <section className=" padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className=" padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className=" padding-x padding-t pb-8 bg-black">
+        <Footer />
+      </section>
     </main>
   );
 };
