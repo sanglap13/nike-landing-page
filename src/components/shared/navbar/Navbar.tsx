76f44787624ad71c3dc15217536c997221750fc8
Sanglap Mridha <sanglapmridha@gmail.com>
2024-03-07 16:33:50 +0530

    feat: add navbar

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { headerLogo } from "../../../assets/images";
+import { hamburger } from "../../../assets/icons";
+import { NAVBAR_LINKS } from "../../../constants/navbarLinks";
 
 const Navbar = () => {
-  return <div>Navbarr</div>;
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img
+            src={headerLogo}
+            alt="logo"
+            width={129}
+            height={29}
+            className="m-0 w-[129px] h-[29px]"
+          />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {NAVBAR_LINKS.map((link) => (
+            <li key={link.label}>
+              <a
+                href={link.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+          <a href="/">Sign in</a>
+          <span>/</span>
+          <a href="/">Explore now</a>
+        </div>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger icon" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
---
+++ src/constants/navbarLinks.ts
@@ -0,0 +1,6 @@
+export const NAVBAR_LINKS = [
+  { href: "#home", label: "Home" },
+  { href: "#about-us", label: "About Us" },
+  { href: "#products", label: "Products" },
+  { href: "#contact-us", label: "Contact Us" },
+];
