385891b4160188e26352d3443e3f02b3034b1065
Sanglap Mridha <sanglapmridha@gmail.com>
2024-03-07 18:55:14 +0530

    feat: add services section

--- src/components/sections/services/Services.tsx
+++ src/components/sections/services/Services.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { SERVICES } from "../../../constants/services";
+import { ServiceCard } from "../../shared";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {SERVICES.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -3,3 +3,4 @@ export { default as Navbar } from "./navbar/Navbar";
 export { default as Button } from "./button/Button";
 export { default as ShoeCard } from "./shoeCard/ShoeCard";
 export { default as PopularProductCard } from "./popularProductCard/PopularProductCard";
+export { default as ServiceCard } from "./serviceCard/ServiceCard";
