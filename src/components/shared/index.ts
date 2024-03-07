60531f0ee12ec24fa948d80a959448febd8340b5
Sanglap Mridha <sanglapmridha@gmail.com>
2024-03-07 18:34:18 +0530

    feat: add popularProducts Section

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,3 +2,4 @@ export { default as Navbar } from "./navbar/Navbar";
 
 export { default as Button } from "./button/Button";
 export { default as ShoeCard } from "./shoeCard/ShoeCard";
+export { default as PopularProductCard } from "./popularProductCard/PopularProductCard";
---
+++ src/constants/products.ts
@@ -0,0 +1,24 @@
+import { shoe4, shoe5, shoe6, shoe7 } from "../assets/images";
+
+export const PRODUCTS = [
+  {
+    imgURL: shoe4,
+    name: "Nike Air Jordan-01",
+    price: "$200.20",
+  },
+  {
+    imgURL: shoe5,
+    name: "Nike Air Jordan-10",
+    price: "$210.20",
+  },
+  {
+    imgURL: shoe6,
+    name: "Nike Air Jordan-100",
+    price: "$220.20",
+  },
+  {
+    imgURL: shoe7,
+    name: "Nike Air Jordan-001",
+    price: "$230.20",
+  },
+];
