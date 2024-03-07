48f85fa241f30826135cddfc938435cda4459128
Sanglap Mridha <sanglapmridha@gmail.com>
2024-03-07 18:11:34 +0530

    feat: add hero section

---
+++ src/constants/heroContent.ts
@@ -0,0 +1,29 @@
+import {
+  bigShoe1,
+  bigShoe2,
+  bigShoe3,
+  thumbnailShoe1,
+  thumbnailShoe2,
+  thumbnailShoe3,
+} from "../assets/images";
+
+export const HERO_STATS = [
+  { value: "1k+", label: "Brands" },
+  { value: "500+", label: "Shops" },
+  { value: "250k+", label: "Customers" },
+];
+
+export const SHOES = [
+  {
+    thumbnail: thumbnailShoe1,
+    bigShoe: bigShoe1,
+  },
+  {
+    thumbnail: thumbnailShoe2,
+    bigShoe: bigShoe2,
+  },
+  {
+    thumbnail: thumbnailShoe3,
+    bigShoe: bigShoe3,
+  },
+];
