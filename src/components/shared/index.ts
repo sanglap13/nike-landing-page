3276286fe8b94868b728d69b1ae7b747ba1d31d2
Sanglap Mridha <sanglapmridha@gmail.com>
2024-03-07 19:08:15 +0530

    feat: add customerReview section

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -4,3 +4,4 @@ export { default as Button } from "./button/Button";
 export { default as ShoeCard } from "./shoeCard/ShoeCard";
 export { default as PopularProductCard } from "./popularProductCard/PopularProductCard";
 export { default as ServiceCard } from "./serviceCard/ServiceCard";
+export { default as ReviewCard } from "./reviewCard/ReviewCard";
---
+++ src/constants/customerReviews.ts
@@ -0,0 +1,18 @@
+import { customer1, customer2 } from "../assets/images";
+
+export const CUSTOMER_REVIEWS = [
+  {
+    imgURL: customer1,
+    customerName: "Morich Brown",
+    rating: 4.5,
+    feedback:
+      "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!",
+  },
+  {
+    imgURL: customer2,
+    customerName: "Lota Mongeskar",
+    rating: 4.5,
+    feedback:
+      "The product not only met but exceeded my expectations. I'll definitely be a returning customer!",
+  },
+];
